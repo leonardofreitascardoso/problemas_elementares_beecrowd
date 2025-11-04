9994344c8ecb857ebd7a4a7226abd22e1eff0020
Leonardo Freitas Cardoso <cardosofreitas33@gmail.com>
2025-11-04 15:54:23 -0300

    Update divisao_X_por_Y.py

--- divisao_X_por_Y.py
+++ divisao_X_por_Y.py
@@ -1,3 +1,19 @@
+##beecrowd | 1116
+##-------------------------------------------------------------
+
+
+##V1: exibe o resultado na medida em que os pares são digitados
+n = int(input())
+
+for _ in range(n):
+    x, y = map(int,input().split())
+
+    if y == 0:
+        print('divisao impossivel')
+    else:
+        print(f'{x/y:.1f}')
+
+##V2: exibe o resultado somente ao fim
 pares = int(input())
 lista = []
 contador = 0
@@ -11,4 +27,3 @@ for item in lista:
         print('divisao impossivel')
     else:
         print(int(item[0])/int(item[1]))
-    
